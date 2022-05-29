90ce14676853293b07ab844a5e784ca940ce394b
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-05-30 00:27:08 +0200

    updated token name (#2)

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Julius {
+contract MyToken is ERC20 {
 
-    constructor() ERC20("Julius", "NOVA") {
+    constructor() ERC20("MyToken", "MTN") {
         _mint(msg.sender, 1000000 * 10 ** 18);
     }
 }
