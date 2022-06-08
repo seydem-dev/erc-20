0babc0709a8de1e741b5e3bf145e7e47c1f00f9e
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-06-08 19:57:37 +0200

    init

--- GoldToken.sol
+++ GoldToken.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
+contract GoldToken is ERC20 {
 
-    constructor() ERC20("MyToken", "MTN") {
+    constructor() ERC20("Gold Token", "GOLD") {
         _mint(msg.sender, 1000000 * 10 ** 18);
     }
 }
