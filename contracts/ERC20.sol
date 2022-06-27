24a7840d363b271224f466d7ebcad46c550bc098
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-06-27 21:36:48 +0200

    Update

---
+++ contracts/ERC20.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.6.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+
+contract ERC20 is ERC20, Pausable, Ownable {
+    constructor() ERC20("ERC20", "ERCT") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal whenNotPaused {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
