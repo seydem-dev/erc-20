ca9744e4d197faa716303b3b0fd9027ff07572f6
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-06-28 15:55:49 +0200

    Update

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.6.0/security/Pausable.sol";
-import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ERC20 is ERC20, Pausable, Ownable {
 
