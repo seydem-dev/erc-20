b7b9e5bab7a2fdc54a82ebb071fbbadaeea0aec4
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-03 11:49:19 +0200

    Remove and add functions

--- contracts/Diamonds.sol
+++ contracts/Diamonds.sol
@@ -3,20 +3,15 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Diamonds is ERC20("Diamonds", "DIAS"), Pausable, Ownable {
-    
-     function pause() external onlyOwner {
-        _pause();
+contract Diamonds is ERC20("Diamonds", "DIAS"), Ownable {
+
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
     }
 
-    function unpause() external onlyOwner {
-        _unpause();
-    }
-
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
+    function burn(address account, uint256 amount) external onlyOwner {
+        _burn(account, amount);
     }
 }
