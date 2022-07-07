4fdaa8972c6a7a7493049eef84bb56a78610949a
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-07 15:24:59 +0200

    Add ERC20Burnable

--- contracts/Diamonds.sol
+++ contracts/Diamonds.sol
@@ -3,17 +3,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Diamonds is ERC20("Diamonds", "DIAS"), Ownable, Pausable {
+contract Diamonds is ERC20("Diamonds", "DIAS"), ERC20Burnable, Pausable, Ownable {
 
     function mint(address account, uint256 amount) external whenNotPaused {
         _mint(account, amount);
-    }
-
-    function burn(address account, uint256 amount) external whenNotPaused {
-        _burn(account, amount);
     }
 
     function pause() external onlyOwner {
