99ea881f8709606eb016fdfcebd96140eb293145
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-10 04:47:58 +0200

    Rename contract

---
+++ contracts/Banana.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Banana is ERC20("Banana", "BANANA"), ERC20Burnable, Pausable, Ownable {
+
+    function mint(address account, uint256 amount) external whenNotPaused {
+        _mint(account, amount);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+}
